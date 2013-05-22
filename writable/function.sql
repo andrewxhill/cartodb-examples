058fec71be49246b40680123374a509f54f076a4
Andrew Hill <andrewxhill@gmail.com>
2013-05-22 11:45:25 -0500

    polygon shape viewer

--- writable/function.sql
+++ writable/function.sql
@@ -1,6 +1,6 @@
 -- Create a function withn the security set to Definer so that it can insert
 CREATE OR REPLACE FUNCTION AXH_NewMinneapolis(integer, geometry) RETURNS integer
-	AS 'INSERT INTO minneapolis (parent_id, the_geom) VALUES($1,$2) RETURNING cartodb_id;'
+	AS 'INSERT INTO minneapolis (parent_id, the_geom) (SELECT id, geom FROM (SELECT $1 id,$2 geom) a WHERE ST_NPoints(geom) < 100) RETURNING cartodb_id;'
 	LANGUAGE SQL 
 	SECURITY DEFINER
 	RETURNS NULL ON NULL INPUT;
