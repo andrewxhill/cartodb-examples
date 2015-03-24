846db7bf5ee9604931fdd152eba7031097c06b5e
andrewxhill <andrewxhill@gmail.com>
2015-03-24 15:05:39 -0400

    fixes for new lib

--- scroll-story/basic/js/app.js
+++ scroll-story/basic/js/app.js
@@ -63,33 +63,33 @@ function legendup(data){
 }
 
 cartodb.createLayer(map, {
+  user_name: 'osm2',
   type: 'cartodb',
-  options: {
-      table_name: 'building_comments_1854',
-      user_name: 'osm2',
+  sublayers: [{
       interaction: false,
-      query: "SELECT the_geom_webmercator FROM buildings_1854",
-      interaction: false,
-      tile_style: "#{{table_name}}{ polygon-fill:white;polygon-opacity: 0.2;line-width: 0;}",
-      extra_params: { cache_buster: "rambo_is_the_best" }
-      }
+      sql: "SELECT the_geom_webmercator FROM buildings_1854",
+      cartocss:  "#buildings_1854{ polygon-fill:white;polygon-opacity: 0.2;line-width: 0;}"
+    }]
   })
+ .addTo(map)
  .on('done', function(layer) {
-    map.setView(position, zoom)
-      .addLayer(layer);
+    map.setView(position, zoom);
     layer.hide();
     mapLayers.push(layer)
 
-    cartodb.createLayer(map, 'http://osm2.cartodb.com/api/v1/viz/building_comments_1854/viz.json', {
-        query: "SELECT *, 1 as layer FROM buildings_1854 WHERE cartodb_id IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) AND  cartodb_id IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL) union all SELECT *, 2 as layer FROM buildings_1854 WHERE cartodb_id IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) AND cartodb_id NOT IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL) UNION ALL SELECT *, 3 as layer FROM buildings_1854 WHERE cartodb_id IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL) AND cartodb_id NOT IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) UNION ALL SELECT *, 4 as layer FROM buildings_1854 WHERE cartodb_id NOT IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) AND  cartodb_id NOT IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL)",
+    cartodb.createLayer(map, {
+      user_name: 'osm2',
+      type: 'cartodb',
+      sublayers: [{
+        sql: "SELECT *, 1 as layer FROM buildings_1854 WHERE cartodb_id IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) AND  cartodb_id IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL) union all SELECT *, 2 as layer FROM buildings_1854 WHERE cartodb_id IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) AND cartodb_id NOT IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL) UNION ALL SELECT *, 3 as layer FROM buildings_1854 WHERE cartodb_id IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL) AND cartodb_id NOT IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) UNION ALL SELECT *, 4 as layer FROM buildings_1854 WHERE cartodb_id NOT IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) AND  cartodb_id NOT IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL)",
         interaction: false,
-        tile_style: "#{{table_name}}{polygon-fill: white;  polygon-opacity: 0.2;  line-width: 0;  line-color: #FFF;  line-opacity: 1;  [layer = 1] {   polygon-fill: violet;  polygon-opacity: 1;  }  [layer = 2] {   polygon-fill: red;  polygon-opacity: 0.7;  }  [layer = 3] {   polygon-fill: yellow;  polygon-opacity: 0.7;  }}",
-        extra_params: { cache_buster: "rambo_is_the_best" }
+        cartocss:  "#buildings_1854{polygon-fill: white;  polygon-opacity: 0.2;  line-width: 0;  line-color: #FFF;  line-opacity: 1;  [layer = 1] {   polygon-fill: violet;  polygon-opacity: 1;  }  [layer = 2] {   polygon-fill: red;  polygon-opacity: 0.7;  }  [layer = 3] {   polygon-fill: yellow;  polygon-opacity: 0.7;  }}",
+      }]
       })
+     .addTo(map)
      .on('done', function(layer2) {
-      map.addLayer(layer2);
       mapLayers.push(layer2);
-updateViz(currentViz);
+      updateViz(currentViz);
     layer.hide();
     }).on('error', function() {
       cartodb.log.log("some error occurred");
@@ -116,9 +116,9 @@ var viz = {
     setTimeout(function(){
       mapLayers[0].hide();
       mapLayers[1].setOptions({
-            query: "SELECT *, 1 as layer FROM buildings_1854 WHERE cartodb_id IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) AND  cartodb_id IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL) union all SELECT *, 2 as layer FROM buildings_1854 WHERE cartodb_id IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) AND cartodb_id NOT IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL) UNION ALL SELECT *, 3 as layer FROM buildings_1854 WHERE cartodb_id IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL) AND cartodb_id NOT IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) UNION ALL SELECT *, 4 as layer FROM buildings_1854 WHERE cartodb_id NOT IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) AND  cartodb_id NOT IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL)",
+            sql: "SELECT *, 1 as layer FROM buildings_1854 WHERE cartodb_id IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) AND  cartodb_id IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL) union all SELECT *, 2 as layer FROM buildings_1854 WHERE cartodb_id IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) AND cartodb_id NOT IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL) UNION ALL SELECT *, 3 as layer FROM buildings_1854 WHERE cartodb_id IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL) AND cartodb_id NOT IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) UNION ALL SELECT *, 4 as layer FROM buildings_1854 WHERE cartodb_id NOT IN (SELECT building_id from directory_1854 WHERE building_id IS NOT NULL) AND  cartodb_id NOT IN (SELECT building_id from directory_1839 WHERE building_id IS NOT NULL)",
             interaction: false,
-            tile_style: "#{{table_name}}{polygon-fill: white;  polygon-opacity: 0.2;  line-width: 0;  line-color: #FFF;  line-opacity: 1;  [layer = 1] {   polygon-fill: violet;  polygon-opacity: 1;  }  [layer = 2] {   polygon-fill: red;  polygon-opacity: 0.7;  }  [layer = 3] {   polygon-fill: yellow;  polygon-opacity: 0.7;  }}"
+            cartocss:  "#{{table_name}}{polygon-fill: white;  polygon-opacity: 0.2;  line-width: 0;  line-color: #FFF;  line-opacity: 1;  [layer = 1] {   polygon-fill: violet;  polygon-opacity: 1;  }  [layer = 2] {   polygon-fill: red;  polygon-opacity: 0.7;  }  [layer = 3] {   polygon-fill: yellow;  polygon-opacity: 0.7;  }}"
       });
     }, 500)
   },
@@ -137,9 +137,9 @@ var viz = {
     setTimeout(function(){
       mapLayers[0].hide();
       mapLayers[1].setOptions({
-            query: "SELECT * FROM buildings_1854",
+            sql: "SELECT * FROM buildings_1854",
             interaction: false,
-            tile_style: "#{{table_name}}{  polygon-fill: white;  polygon-opacity: 0.2;  line-width: 0;  line-color: #FFF;  line-opacity: 0.6;  [type='shingle roof']{   polygon-fill: #fc0b82;    polygon-opacity: 0.7;  }  [type='slate or metal']{  polygon-fill: #4a8dcb;    polygon-opacity: 0.7;  }  [type='wood or framed'], [type='framed dwelling']{  polygon-fill: #f2b357;    polygon-opacity: 0.7;  }  [type='brick or stone']{  polygon-fill: #92d42d;    polygon-opacity: 0.7;  }  [hazardous=true]{   line-width: 0.4;    line-color: #FF0000;  }}"
+            cartocss:  "#{{table_name}}{  polygon-fill: white;  polygon-opacity: 0.2;  line-width: 0;  line-color: #FFF;  line-opacity: 0.6;  [type='shingle roof']{   polygon-fill: #fc0b82;    polygon-opacity: 0.7;  }  [type='slate or metal']{  polygon-fill: #4a8dcb;    polygon-opacity: 0.7;  }  [type='wood or framed'], [type='framed dwelling']{  polygon-fill: #f2b357;    polygon-opacity: 0.7;  }  [type='brick or stone']{  polygon-fill: #92d42d;    polygon-opacity: 0.7;  }  [hazardous=true]{   line-width: 0.4;    line-color: #FF0000;  }}"
       });
     }, 500)
   },
@@ -162,9 +162,9 @@ var viz = {
 
       mapLayers[0].hide();
       mapLayers[1].setOptions({
-            query: "SELECT * FROM buildings_1854",
+            sql: "SELECT * FROM buildings_1854",
             interaction: false,
-            tile_style: "#{{table_name}}{  polygon-fill: white;  polygon-opacity: 0.2;  line-width: 0;  line-color: #FFF;  line-opacity: 0.6;  [type='shingle roof']{   polygon-fill: #fc0b82;    polygon-opacity: 0.7;  }  [type='slate or metal']{  polygon-fill: #4a8dcb;    polygon-opacity: 0.7;  }  [type='wood or framed'], [type='framed dwelling']{  polygon-fill: #f2b357;    polygon-opacity: 0.7;  }  [type='brick or stone']{  polygon-fill: #92d42d;    polygon-opacity: 0.7;  }  [hazardous=true]{   line-width: 0.4;    line-color: #FF0000;  }}"
+            cartocss:  "#{{table_name}}{  polygon-fill: white;  polygon-opacity: 0.2;  line-width: 0;  line-color: #FFF;  line-opacity: 0.6;  [type='shingle roof']{   polygon-fill: #fc0b82;    polygon-opacity: 0.7;  }  [type='slate or metal']{  polygon-fill: #4a8dcb;    polygon-opacity: 0.7;  }  [type='wood or framed'], [type='framed dwelling']{  polygon-fill: #f2b357;    polygon-opacity: 0.7;  }  [type='brick or stone']{  polygon-fill: #92d42d;    polygon-opacity: 0.7;  }  [hazardous=true]{   line-width: 0.4;    line-color: #FF0000;  }}"
           });
     },500);
   },
@@ -179,9 +179,9 @@ var viz = {
     setTimeout(function(){
       mapLayers[0].show();
       mapLayers[1].setOptions({
-                  query: "WITH nms AS (SELECT name FROM (SELECT count(distinct directory) d, count(distinct building_id) b, name, profession FROM directory_names WHERE profession != '' group by name, profession) a WHERE d = 2 AND b = 2), f AS (SELECT name_id, building_id, lower(trim(both ' ' from profession)) as profession FROM directory_1854 WHERE name_id in (SELECT name FROM nms)) SELECT ST_Transform((SELECT ST_Centroid(the_geom) FROM buildings_1854 WHERE cartodb_id = f.building_id),3857) as the_geom_webmercator, f.name_id, f.profession FROM f",
+                  sql: "WITH nms AS (SELECT name FROM (SELECT count(distinct directory) d, count(distinct building_id) b, name, profession FROM directory_names WHERE profession != '' group by name, profession) a WHERE d = 2 AND b = 2), f AS (SELECT name_id, building_id, lower(trim(both ' ' from profession)) as profession FROM directory_1854 WHERE name_id in (SELECT name FROM nms)) SELECT ST_Transform((SELECT ST_Centroid(the_geom) FROM buildings_1854 WHERE cartodb_id = f.building_id),3857) as the_geom_webmercator, f.name_id, f.profession FROM f",
                   interaction: false,
-                  tile_style: "Map{buffer-size: 150;} #{{table_name}}{text-name: [profession];text-face-name: 'DejaVu Sans Book'; text-size: 6; text-fill: #8cc640; text-allow-overlap: false; text-halo-fill: #FFF; text-halo-radius: 0.3; [zoom>13]{text-size: 8;[zoom>14]{text-size: 10;[zoom>15]{text-size: 14;}}}}"
+                  cartocss:  "Map{buffer-size: 150;} #{{table_name}}{text-name: [profession];text-face-name: 'DejaVu Sans Book'; text-size: 6; text-fill: #8cc640; text-allow-overlap: false; text-halo-fill: #FFF; text-halo-radius: 0.3; [zoom>13]{text-size: 8;[zoom>14]{text-size: 10;[zoom>15]{text-size: 14;}}}}"
               });
     },500);
   },
@@ -200,8 +200,8 @@ var viz = {
     setTimeout(function(){
       mapLayers[0].show();
       mapLayers[1].setOptions({
-                query: "WITH  nms AS (SELECT name FROM (SELECT count(distinct directory) d, count(distinct building_id) b, name, profession FROM directory_names WHERE profession != '' group by name, profession) a WHERE d = 2 AND b = 2), f AS (SELECT name_id, building_id, lower(trim(both ' ' from occupation)) as profession FROM directory_1839 WHERE name_id in (SELECT name FROM nms)), m AS (SELECT name_id, building_id, lower(trim(both ' ' from profession)) as profession FROM directory_1854 WHERE name_id in (SELECT name FROM nms)), t AS (SELECT (SELECT st_centroid(the_geom_webmercator) FROM buildings_1854 WHERE cartodb_id = f.building_id) the_geom_webmercator, 'source' as layer, f.name_id, f.profession FROM f UNION ALL SELECT (SELECT st_centroid(the_geom_webmercator) FROM buildings_1854 WHERE cartodb_id = m.building_id) the_geom_webmercator, 'dest' as layer, m.name_id, m.profession FROM m) SELECT * FROM t UNION ALL SELECT ST_MakeLine(the_geom_webmercator) the_geom_webmercator, 'line' as layer, name_id, profession FROM t GROUP BY name_id, profession",
-                tile_style: "#{{table_name}}{[layer='line'] {line-color: white; line-width: 0.8; line-opacity: 0.3;}[layer!='line']::top {marker-fill:blue;marker-width: 4; marker-allow-overlap: true;marker-opacity: 0.7; line-opacity:1; line-width: 0; line-color: #FFFFFF; [layer = 'source']{ marker-opacity: 0.7; marker-fill:red;}}}"
+                sql: "WITH  nms AS (SELECT name FROM (SELECT count(distinct directory) d, count(distinct building_id) b, name, profession FROM directory_names WHERE profession != '' group by name, profession) a WHERE d = 2 AND b = 2), f AS (SELECT name_id, building_id, lower(trim(both ' ' from occupation)) as profession FROM directory_1839 WHERE name_id in (SELECT name FROM nms)), m AS (SELECT name_id, building_id, lower(trim(both ' ' from profession)) as profession FROM directory_1854 WHERE name_id in (SELECT name FROM nms)), t AS (SELECT (SELECT st_centroid(the_geom_webmercator) FROM buildings_1854 WHERE cartodb_id = f.building_id) the_geom_webmercator, 'source' as layer, f.name_id, f.profession FROM f UNION ALL SELECT (SELECT st_centroid(the_geom_webmercator) FROM buildings_1854 WHERE cartodb_id = m.building_id) the_geom_webmercator, 'dest' as layer, m.name_id, m.profession FROM m) SELECT * FROM t UNION ALL SELECT ST_MakeLine(the_geom_webmercator) the_geom_webmercator, 'line' as layer, name_id, profession FROM t GROUP BY name_id, profession",
+                cartocss:  "#{{table_name}}{[layer='line'] {line-color: white; line-width: 0.8; line-opacity: 0.3;}[layer!='line']::top {marker-fill:blue;marker-width: 4; marker-allow-overlap: true;marker-opacity: 0.7; line-opacity:1; line-width: 0; line-color: #FFFFFF; [layer = 'source']{ marker-opacity: 0.7; marker-fill:red;}}}"
               })
     },500);
   },
@@ -221,8 +221,8 @@ var viz = {
       mapLayers[0].show();
       mapLayers[1].setOptions({
                   interaction: false,
-                  query: "WITH  nms AS (SELECT name FROM (SELECT count(distinct directory) d, count(distinct building_id) b, name, profession FROM directory_names WHERE profession != '' group by name, profession) a WHERE d = 2 AND b = 2), f AS (SELECT name_id, building_id, lower(trim(both ' ' from occupation)) as profession FROM directory_1839 WHERE name_id = 'stokell john' AND name_id in (SELECT name FROM nms)), m AS (SELECT name_id, building_id, lower(trim(both ' ' from profession)) as profession FROM directory_1854 WHERE name_id = 'stokell john' AND name_id in (SELECT name FROM nms)), t AS (SELECT (SELECT st_centroid(the_geom_webmercator) FROM buildings_1854 WHERE cartodb_id = f.building_id) the_geom_webmercator, 'source' as layer, f.name_id, f.profession FROM f UNION ALL SELECT (SELECT st_centroid(the_geom_webmercator) FROM buildings_1854 WHERE cartodb_id = m.building_id) the_geom_webmercator, 'dest' as layer, m.name_id, m.profession FROM m) SELECT * FROM t UNION ALL SELECT ST_MakeLine(the_geom_webmercator) the_geom_webmercator, 'line' as layer, name_id, profession FROM t GROUP BY name_id, profession",
-                  tile_style: "#{{table_name}}{[layer='line'] {line-color: white; line-width: 0.8; line-opacity: 0.3;}[layer!='line']::top {marker-fill:blue;marker-width: 10;marker-allow-overlap: true;marker-opacity: 0.7; line-opacity:1; line-width: 0; line-color: #FFFFFF; [layer = 'source']{ marker-opacity: 0.7; marker-fill:red;}}}"
+                  sql: "WITH  nms AS (SELECT name FROM (SELECT count(distinct directory) d, count(distinct building_id) b, name, profession FROM directory_names WHERE profession != '' group by name, profession) a WHERE d = 2 AND b = 2), f AS (SELECT name_id, building_id, lower(trim(both ' ' from occupation)) as profession FROM directory_1839 WHERE name_id = 'stokell john' AND name_id in (SELECT name FROM nms)), m AS (SELECT name_id, building_id, lower(trim(both ' ' from profession)) as profession FROM directory_1854 WHERE name_id = 'stokell john' AND name_id in (SELECT name FROM nms)), t AS (SELECT (SELECT st_centroid(the_geom_webmercator) FROM buildings_1854 WHERE cartodb_id = f.building_id) the_geom_webmercator, 'source' as layer, f.name_id, f.profession FROM f UNION ALL SELECT (SELECT st_centroid(the_geom_webmercator) FROM buildings_1854 WHERE cartodb_id = m.building_id) the_geom_webmercator, 'dest' as layer, m.name_id, m.profession FROM m) SELECT * FROM t UNION ALL SELECT ST_MakeLine(the_geom_webmercator) the_geom_webmercator, 'line' as layer, name_id, profession FROM t GROUP BY name_id, profession",
+                  cartocss:  "#{{table_name}}{[layer='line'] {line-color: white; line-width: 0.8; line-opacity: 0.3;}[layer!='line']::top {marker-fill:blue;marker-width: 10;marker-allow-overlap: true;marker-opacity: 0.7; line-opacity:1; line-width: 0; line-color: #FFFFFF; [layer = 'source']{ marker-opacity: 0.7; marker-fill:red;}}}"
                 })
     },500);
   },
@@ -234,9 +234,9 @@ var viz = {
     setTimeout(function(){
         mapLayers[0].hide();
         mapLayers[1].setOptions({
-                query: "SELECT * FROM buildings_1854",
+                sql: "SELECT * FROM buildings_1854",
                 interaction: false,
-                tile_style: "#{{table_name}}{polygon-fill:white; polygon-opacity: 0.2; line-opacity:1; line-width: 0; line-color: #FFFFFF; [hazardous = true]{ polygon-opacity: 0.7; polygon-fill:red;}}"
+                cartocss:  "#{{table_name}}{polygon-fill:white; polygon-opacity: 0.2; line-opacity:1; line-width: 0; line-color: #FFFFFF; [hazardous = true]{ polygon-opacity: 0.7; polygon-fill:red;}}"
             })
       },500);
   }
